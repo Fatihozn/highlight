1ecf0af47a39e8c25fab18fbd44dbf804c89d432
Fatihozn <100231583+Fatihozn@users.noreply.github.com>
2022-08-23 16:53:59 +0300

    clean code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+export default function Light({ cumle, text }) {
+  const highlight = (cumle, text) => {
+    text = text.toLowerCase();
+    const splitedCumle = cumle.split("");
+    const lowerCaseSplitedCumle = cumle.toLowerCase().split("");
+    let enter = true;
+    const indexArray = [];
+    let htmlText = "";
+
+    for (let i = 0; i < lowerCaseSplitedCumle.length; i++) {
+      enter = true;
+      for (let j = 0; j < text.length; j++) {
+        if (lowerCaseSplitedCumle[i + j] !== text[j]) {
+          enter = false;
+          break;
+        }
+      }
+      if (enter) {
+        for (let j = 0; j < text.length; j++) {
+          indexArray.push(i + j);
+        }
+      }
+    }
+
+    for (let i = 0; i < splitedCumle.length; i++) {
+      if (indexArray.includes(i)) {
+        htmlText += `<span style="background-color: yellow">${splitedCumle[i]}</span>`;
+      } else {
+        htmlText += splitedCumle[i];
+      }
+    }
+    return { __html: htmlText };
+  };
+
+  return <div dangerouslySetInnerHTML={highlight(cumle, text)}></div>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const cumle =
+  "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.";
+const text = "lorem ipsum";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(<Light cumle={cumle} text={text} />);
